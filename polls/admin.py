dd2cc57d6cb1f15a5fce4dd5cdc526fd5d852c44
App Academy Student <student@appacademy.io>
2015-08-17 15:31:10 -0700

    Add templates folder; finish workign with Question and Choice models

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,5 +13,9 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,              {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
